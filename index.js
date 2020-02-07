dab6e13e35a9ed76e4c26d2bc828b4828c1f3569
meytal kozlovsky <meytalkozlovsky@gmail.com>
2020-02-07 23:08:33 +0200

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-var http = require('http');
-http.createServer(function(request, response) {  
+var https = require('https');
+https.createServer(function(request, response) {  
         response.writeHead(200, {"Content-Type": "text/html"});  
         response.write('<p>hello world</p>');
         response.end();  
-    }).listen(8080);
+    }).listen(443);
