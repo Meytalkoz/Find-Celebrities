eedf2381fd69762d77d94ada6279e571c6871c5a
meytal kozlovsky <meytalkozlovsky@gmail.com>
2020-02-07 18:33:11 +0200

    test server

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var http = require('http');
+http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write('<p>hello world</p>');
+        response.end();  
+    }).listen(8000);
