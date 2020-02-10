3f599ed82c97b4744d8475970330bf6b087f69ea
meytal kozlovsky <meytalkozlovsky@gmail.com>
2020-02-10 18:21:08 +0200

    removed refundant file

--- index.js
+++
@@ -1,7 +0,0 @@
-
-var https = require('https');
-https.createServer(function(request, response) {  
-        response.writeHead(200, {"Content-Type": "text/html"});  
-        response.write('<p>hello world</p>');
-        response.end();  
-    }).listen(443);
