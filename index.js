c07d719302127670f64afb3f1cdd41ee5a40b2c0
Meytal Kozlovsky <Meytalkozlovsky@gmail.com>
2020-02-11 15:30:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 /*
- * Index logic, contain all the app lifecycle. Using express as server hadler
+ * Index logic, contain all the app lifecycle. Defines port: 443 and protocol: https
  */
 
-var fs = require('fs'),
-    express = require('express'),
+var https = require('https'),
+    fs = require('fs'),
     celebrityHandler = require('./bingSearchHandler'),
     storageHandler = require('./azureDBHandler'),
     UiHandler = require('./UiHandler');
 
 
-    const app = express()
-    const port = process.env.PORT || 80
-    app.get('/', (req, res) => {
-        fs.readFile('./index.html', function (err, html) {
-            if (err) {
-                throw err;
-            }            
-            responseToClient(req, res, html);                   
-        });    
-    });
-    app.listen(port, () => console.log(`Server is listening on port: ${port}`))
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+
+    https.createServer(function (request, response) {
+        responseToClient(request, response, html);
+    }).listen(443);
+
+});
 
 async function responseToClient(request, response, html) {
     response.writeHead(200, { "Content-Type": "text/html" });
