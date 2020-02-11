a4934edbfa8e9db02aafb1a1beaa5e8d023f7b45
Meytal Kozlovsky <Meytalkozlovsky@gmail.com>
2020-02-11 15:14:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Index logic, contain all the app lifecycle. Defines port: 443 and protocol: https
  */
 
-var https = require('http'),
+var https = require('https'),
     fs = require('fs'),
     celebrityHandler = require('./bingSearchHandler'),
     storageHandler = require('./azureDBHandler'),
